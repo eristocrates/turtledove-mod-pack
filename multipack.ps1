c9f3a0372b9a2a4d53ecf741a520e045c4082cdd
eristocrates <reillocb@gmail.com>
2025-05-20 17:00:57 -0400

    add launch configuration for multipack and update multipack script to handle mod URLs remove obsolete mod JAR files create mods.txt and mods.txt.done for tracking mod sources

--- multipack.ps1
+++ multipack.ps1
@@ -1,13 +1,59 @@
-$base = Get-Location
-$client = "\turtledove-client-mod-pack"
-$server = "\turtledove-server-mod-pack"
-$directories = @( "$base$client", "$base$server" )
+$div = '\'
+$root = Get-Location
+$base = "$root$div"
+$client = "turtledove-client-mod-pack"
+$server = "turtledove-server-mod-pack"
 
-$refresh = "packwiz refresh"
+$modsBase = "mods.txt"
+$clientBase = "client.txt"
+$serverBase = "server.txt"
+
+$last = "last"
+
+$modsFile = "$base$modsBase"
+$clientFile = "$base$clientBase"
+$serverFile = "$base$serverBase"
+
+$directories = @( 
+    "$base$client",
+    "$base$server"
+)
+
+$files = @( 
+    "$base$modsFile",
+    "$base$clientFile",
+    "$base$serverFile"
+)
+
 $rebuild = "packwiz refresh --build"
+$modrinth = "packwiz modrinth add"
+$curseforge = "packwiz curseforge add"
 
-foreach ($directory in $directories) {
-    Invoke-Expression -Command "cd $directory"
-    Invoke-Expression -Command $rebuild
+# Check if the file exists
+if (Get-Item -Path $modsFile) {
+    # Read all lines from the file
+    $urls = Get-Content $modsFile
+    foreach ($directory in $directories) {
+        Set-Location $directory
+        foreach ($url in $urls) {
+            # Trim any whitespace from the url path
+            $url = $url.Trim()
+            if ($url -like "*modrinth*") { 
+                Invoke-Expression -Command "echo '$modrinth $url'" 
+                Invoke-Expression -Command "$modrinth $url"
+            }
+            if ($url -like "*curseforge*") {
+                Invoke-Expression -Command "echo '$curseforge $url'" 
+                Invoke-Expression -Command "$curseforge $url"
+            }
+            Invoke-Expression -Command $rebuild
+        }
+    }
+    Clear-Content -Path $modsFile
+    Add-Content -Path "$modsFile.done" -Value $urls
+    Set-Location $root
 }
-Set-Location $base
+else {
+    Write-Host "File not found: $files" -ForegroundColor Red
+}
+
